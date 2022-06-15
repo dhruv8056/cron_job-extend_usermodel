472e3b2cab29431db9f9d676861b094257aa3ec0
Rahul <rb.globalia@gmail.com>
2022-06-15 12:07:46 +0530

    Specify the command to run your project

--- hotel/settings.py
+++ hotel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(0(npbtalj5)gv!=i(s47&zwu9bddo!8=s(@ek)(q0m^9z#%i9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
